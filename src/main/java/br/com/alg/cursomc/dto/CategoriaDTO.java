3798d2a3e797f2f29f037e14f666ff2265d9f586
Andre Girao <andrelgirao22@hotmail.com>
2018-05-21 12:37:58 -0300

    Personalizacao o retorno da validacao

--- src/main/java/br/com/alg/cursomc/dto/CategoriaDTO.java
+++ src/main/java/br/com/alg/cursomc/dto/CategoriaDTO.java
@@ -13,7 +13,7 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	
-	@NotEmpty
+	@NotEmpty(message="Não pode ser vazio")
 	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
